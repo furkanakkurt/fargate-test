70ecae98aad917ef30b3974aa0d5921e8b73b6b9
Furkan Akkurt <furkanakkurt1905@gmail.com>
2022-01-12 14:27:16 +0300

    Add files

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+
+const app = express();
+
+app.get('/', (req, res) => {
+	res.send('Hello world, first image');
+})
+
+app.get('/health', (req, res) => {
+	res.status(200);
+	res.send('health');
+})
+
+app.listen(8080, () => {
+	console.log('listening on 8080');
+})
